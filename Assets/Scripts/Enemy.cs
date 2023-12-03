1534eaa450a6f8c597824a022e5aedc3f4fce49b
RodrigoGangone <rodrigo.gangone@davinci.edu.ar>
2023-12-03 19:30:01 -0300

    cambios

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,32 +1,125 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private List<Node> patrolNodes = new(); // Lista de nodos predeterminados
+    [Header("PATROL")] [SerializeField] private List<Node> patrolNodes = new(); // Lista de nodos predeterminados
     private int _currentNodePatrol = 0;
-
     public float velocidadMovimiento = 2f;
-    
+
+    [Header("FOV")]
+    [SerializeField] private Player _player; //cuando ve al player
+    [SerializeField]
+    LayerMask _obstacle; //layer que interrumpe su vista
+
+    [SerializeField, Range(1, 10)] float _viewRadius;
+    [SerializeField, Range(1, 360)] float _viewAngle;
+
     void Start()
     {
-        //Posicionamos al enemy en el primer nodo de patrullaje
-        transform.position = patrolNodes[_currentNodePatrol].transform.position;
+        //StartCoroutine(CheckFOVRepeatedly()); //Check de FOV en segundo plano
     }
 
     private void FixedUpdate()
     {
         Patrullar();
+        CheckFOV(); //Check de FOV en segundo plano
     }
 
-    void Patrullar()
+    void Patrullar() //TODO: Agregarlo a FSM
     {
-        transform.position = Vector3.MoveTowards(transform.position, patrolNodes[_currentNodePatrol].transform.position, velocidadMovimiento * Time.deltaTime);
-        
+        transform.position = Vector3.MoveTowards(transform.position, patrolNodes[_currentNodePatrol].transform.position,
+            velocidadMovimiento * Time.deltaTime);
+
         if (Vector3.Distance(transform.position, patrolNodes[_currentNodePatrol].transform.position) < 0.1f)
         {
-            // Avanzar al siguiente nodo de la lista
-            _currentNodePatrol = (_currentNodePatrol + 1) % patrolNodes.Count;
+            _currentNodePatrol = (_currentNodePatrol + 1) % patrolNodes.Count; //Siguiente nodo de la lista
         }
     }
+
+    // Codigo Para chequear el FOV
+    IEnumerator CheckFOVRepeatedly()
+    {
+        while (true)
+        {
+            CheckFOV();
+            yield return null; // Espera un frame antes de la siguiente verificación
+        }
+    }
+    
+    void CheckFOV()
+    {
+        _player.ChangeColor(InFieldOfView(_player.transform.position) ? Color.blue : _player.myInitialMaterialColor);
+    }
+
+    bool InFieldOfView(Vector3 endPos)
+    {
+        Vector3 dir = endPos - transform.position;
+        if (dir.magnitude > _viewRadius) return false;
+        if (!InLineOfSight(transform.position, endPos)) return false;
+        if (Vector3.Angle(transform.forward, dir) > _viewAngle / 2) return false;
+        return true;
+    }
+
+    bool InLineOfSight(Vector3 start, Vector3 end)
+    {
+        Vector3 dir = end - start;
+        return !Physics.Raycast(start, dir, dir.magnitude, _obstacle);
+    }
+
+    Vector3 GetAngleFromDir(float angleInDegrees)
+    {
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), 0);
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, _viewRadius);
+
+        Vector3 DirA = GetAngleFromDir(_viewAngle / 2);
+        Vector3 DirB = GetAngleFromDir(-_viewAngle / 2);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position, transform.position + DirA.normalized * _viewRadius);
+        Gizmos.DrawLine(transform.position, transform.position + DirB.normalized * _viewRadius);
+    }
+    
+    
+    /*void CheckFOV()
+    {
+        _player.ChangeColor(InFieldOfView(_player.transform.position) ? Color.blue : _player.myInitialMaterialColor);
+    }
+
+    bool InFieldOfView(Vector3 endPos)
+    {
+        Vector3 dir = endPos - transform.position;
+        if (dir.magnitude > _viewRadius) return false;
+        if (!InLineOfSight(transform.position, endPos)) return false;
+        if (Vector3.Angle(transform.forward, dir) > _viewAngle / 2) return false;
+        return true;
+    }
+
+    bool InLineOfSight(Vector3 start, Vector3 end)
+    {
+        Vector3 dir = end - start;
+        return !Physics.Raycast(start, dir, dir.magnitude, _obstacle);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, _viewRadius);
+
+        Vector3 DirA = GetAngleFromDir(_viewAngle / 2 + transform.eulerAngles.y);
+        Vector3 DirB = GetAngleFromDir(-_viewAngle / 2 + transform.eulerAngles.y);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position, transform.position + DirA.normalized * _viewRadius);
+        Gizmos.DrawLine(transform.position, transform.position + DirB.normalized * _viewRadius);
+    }
+
+    Vector3 GetAngleFromDir(float angleInDegrees)
+    {
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), 0);
+    }*/
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Properties")]
+    public float speed = 3f; // Velocidad de movimiento del jugador
+    Renderer _rend;
+    public Color myInitialMaterialColor;
+    
+    public LayerMask wallLayer; // Layer para las paredes
+
+    private void Awake()
+    {
+        _rend = GetComponent<Renderer>();
+        myInitialMaterialColor = _rend.material.color;
+    }
+
+    private void Update()
+    {
+        // Obtener entrada del teclado
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calcular la dirección del movimiento
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f).normalized;
+
+        // Mover al jugador
+        MovePlayer(movement);
+    }
+
+    private void MovePlayer(Vector3 movement)
+    {
+        // Calcular la posición objetivo
+        Vector3 targetPosition = transform.position + movement * (speed * Time.deltaTime);
+
+        // Verificar si hay colisión con las paredes
+        Collider[] colliders = Physics.OverlapSphere(targetPosition, 0.2f, wallLayer);
+        if (colliders.Length == 0)
+        {
+            // Mover al jugador si no hay colisión con las paredes
+            transform.position = targetPosition;
+        }
+    }
+    
+    public void ChangeColor(Color color) => _rend.material.color = color;
+}
