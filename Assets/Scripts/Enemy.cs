b5d6e6107cb50d2143237736145e55ca8de61796
RodrigoGangone <rodrigo.gangone@davinci.edu.ar>
2023-12-02 23:35:57 -0300

    Movimientos de patrullaje

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,63 +1,32 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] public Transform[] patrolNodes; // Lista de nodos predeterminados
-    private int startNodePatrol = 0;
+    [SerializeField] private List<Node> patrolNodes = new(); // Lista de nodos predeterminados
+    private int _currentNodePatrol = 0;
 
-    public float velocidadMovimiento = 5f;
-    public float distanciaDeteccion = 5f;
+    public float velocidadMovimiento = 2f;
     
-    // Start is called before the first frame update
     void Start()
     {
         //Posicionamos al enemy en el primer nodo de patrullaje
-        transform.position = patrolNodes[startNodePatrol].position;
+        transform.position = patrolNodes[_currentNodePatrol].transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         Patrullar();
     }
-    
+
     void Patrullar()
     {
+        transform.position = Vector3.MoveTowards(transform.position, patrolNodes[_currentNodePatrol].transform.position, velocidadMovimiento * Time.deltaTime);
         
-      // Obtener el nodo actual
-        Transform currentNode = patrolNodes[startNodePatrol];
-
-          /*// Moverse hacia el nodo actual
-        transform.position = Vector3.MoveTowards(transform.position, nodoActual.position, velocidadMovimiento * Time.deltaTime);*/
-
-        // Comprobar si ha llegado al nodo actual
-        if (Vector3.Distance(transform.position, currentNode.position) < 0.3f)
+        if (Vector3.Distance(transform.position, patrolNodes[_currentNodePatrol].transform.position) < 0.1f)
         {
-            // Cambiar al siguiente nodo en la lista de patrulla
-            startNodePatrol = (startNodePatrol + 1) % patrolNodes.Length;
+            // Avanzar al siguiente nodo de la lista
+            _currentNodePatrol = (_currentNodePatrol + 1) % patrolNodes.Count;
         }
-
-        // Realizar raycasts en las cuatro direcciones (Norte, Sur, Este, Oeste)
-        RealizarRaycast(Vector3.up); // Norte
-        RealizarRaycast(Vector3.down); // Sur
-        RealizarRaycast(Vector3.right); // Este
-        RealizarRaycast(Vector3.left); // Oeste
-    }
-    
-    void RealizarRaycast(Vector3 direccion)
-    {
-        // Lanzar un raycast en la dirección especificada
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, direccion, out hit, distanciaDeteccion, LayerMask.GetMask("Node")))
-        {
-            // Si el raycast choca con un nodo, actualizar el nodo actual
-            startNodePatrol = System.Array.IndexOf(patrolNodes, hit.transform);
-        }
-
-        // Dibujar el raycast en la escena para depuración
-        Debug.DrawRay(transform.position, direccion * distanciaDeteccion, Color.red);
     }
 }
